7f6addd120431627c06e091f5ffa192163534699
Tomi Ortiz <tomiortiz4@gmail.com>
2024-05-07 20:39:16 -0300

    LE-13: Creation of observationsType and changes in the names of properties of user model

--- src/interfaces/observations.ts
+++ src/interfaces/observations.ts
@@ -1,10 +1,12 @@
 import mongoose from 'mongoose';
 import { User } from 'src/interfaces/user';
 
+export type ObservationType = 'PUBLIC' | 'PRIVATE';
+
 export interface Observation {
   title: string;
   description: string;
-  type: string;
+  type: ObservationType;
   author: User;
   date: Date;
   users: User[];
